fd8194765a9a0480b608e2465fde68be7ea6c478
Samantha Kopec <skopec25@gmail.com>
2018-07-24 19:49:23 -0500

    Model for games redone

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,12 @@
 class Game < ApplicationRecord
+  include PgSearch
+  pg_search_scope :search_for_best_game, :against => [:player_count, :duration, :box_size, :genre, :card_or_board]
+  def self.make_array
+    array = []
+    @games = self.all
+    @games.each do |game|
+      array << game
+    end
+    array
+  end
 end
