a320065cab82238bb1db997c8e50cee2098274dc
Samantha Kopec <skopec25@gmail.com>
2018-07-26 10:16:27 -0500

    shuffle added for not user input

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,6 +6,7 @@ class GamesController < ApplicationController
   def show
     @games = Game.search_for_best_game("#{params[:player_count]} #{params[:duration].split[0]} #{params[:box_size]} #{params[:genre]} #{params[:card_or_board]}")
     random_array = []
+    @games = Game.all if @games.first.nil?
     @games.each {|game| random_array << game}
     @game = random_array.sample
   end
