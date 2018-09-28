755b8679983369d78274ebfcc7a28000531080b2
Samantha Kopec <skopec25@gmail.com>
2018-09-28 14:33:13 -0500

    made more mobile-friendly

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,3 @@
 class PagesController < ApplicationController
-  # def home
-  #   @games = Game.all
-  #   @games_array = Game.make_array
-  #   @game = Game.first
-  #   # @game = Game.find(params[:id])
-  # end
 
 end
